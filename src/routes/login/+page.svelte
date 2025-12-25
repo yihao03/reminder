0d8b1d7fb1034a4b3d8975383beca1ea68e06d0b
yihao <yihao039@gmail.com>
2025-12-25 15:36:48 +0800

    Move auth logic to page

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,42 +2,36 @@
   import * as Field from "$lib/components/ui/field";
   import { Input } from "$lib/components/ui/input";
   import { Button } from "$lib/components/ui/button";
-  import { loginWithMail } from "$lib/firebase/auth";
+  import type { ActionData } from "./$types";
+  import { enhance } from "$app/forms";
 
-  let email: string = "";
-  let password: string = "";
-  let error: string = "";
-
-  async function handleSubmit(e: Event) {
-    e.preventDefault();
-    error = "";
-
-    try {
-      await loginWithMail(email, password);
-    } catch (err) {
-      error = "Invalid email or password";
-    }
-  }
+  export let form: ActionData;
 </script>
 
 <div class="w-auto max-w-md mx-auto mt-10 space-y-6">
   <div
     class="flex flex-col gap-4 bg-background p-8 rounded-md shadow-md text-center"
   >
-    <form on:submit={handleSubmit}>
+    <form use:enhance method="POST">
       <Field.Group>
         <Field.Legend>Login</Field.Legend>
         <Field.Field>
           <Field.Label>Email</Field.Label>
-          <Input bind:value={email} type="text" placeholder="Email" />
+          <Input
+            name="email"
+            type="email"
+            placeholder="Email"
+            value={form?.email ?? ""}
+            required
+          />
         </Field.Field>
         <Field.Field>
           <Field.Label>Password</Field.Label>
-          <Input bind:value={password} type="password" placeholder="Password" />
+          <Input name="password" type="password" placeholder="Password" />
         </Field.Field>
         <Button type="submit">Login</Button>
-        {#if error}
-          <div class="text-red-500 mt-2">{error}</div>
+        {#if form?.error}
+          <div class="text-red-500 mt-2">{form.error}</div>
         {/if}
       </Field.Group>
     </form>
