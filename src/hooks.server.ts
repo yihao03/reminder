5ca1b2f3f12951ed2609dcc661d00c9aa3941ac6
yihao <yihao039@gmail.com>
2025-12-25 15:38:02 +0800

    Handle user session

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,27 @@
-import type { HandleFetch } from '@sveltejs/kit';
-import { PUBLIC_BACKEND_URL } from '$env/static/public';
+import { BACKEND_URL } from '$env/static/private';
+import { UserSchema } from '$lib/types/auth';
+import type { Handle, HandleFetch } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+  const userSession = event.cookies.get('user');
+  if (userSession) {
+    try {
+      const parsed = JSON.parse(userSession);
+      const user = UserSchema.parse(parsed);
+      event.locals.user = user;
+    } catch (error) {
+      console.error('Failed to parse user session:', error, userSession);
+      event.locals.user = null;
+    }
+  } else {
+    event.locals.user = null;
+  }
+
+  return resolve(event);
+}
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
-  if (request.url.startsWith(PUBLIC_BACKEND_URL)) {
+  if (request.url.startsWith(BACKEND_URL)) {
 
     const token = event.cookies.get('session_token');
 
