abc47f520d875fb67cb292a174e9f693afab10fa
yihao <yihao039@gmail.com>
2025-12-25 15:38:25 +0800

    Add types with zod validation

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -1,9 +1,13 @@
-export type User = {
-  email?: string | null;
-  displayName?: string | null;
-  photoURL?: string | null;
-  uid?: string | null;
-};
+import z from "zod";
+
+export const UserSchema = z.object({
+  email: z.email().nullable(),
+  displayName: z.string().nullable(),
+  photoURL: z.string().nullable(),
+  uid: z.string(),
+});
+
+export type User = z.infer<typeof UserSchema>
 
 export type SessionState = {
   user: User | null;
---
+++ src/lib/types/event.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+import { UserSchema } from "./auth";
+
+export const EventSchema = z.object({
+  id: z.string(),
+  createdAt: z.iso.datetime(),
+  organiser: z.string(),
+  isOnline: z.boolean(),
+  locationName: z.string(),
+  startTime: z.iso.datetime(),
+  endTime: z.iso.datetime(),
+  details: z.string(),
+  eventName: z.string(),
+  registeredUsers: z.array(UserSchema)
+})
+
+export const EventListViewSchema = EventSchema.omit({ createdAt: true, details: true, registeredUsers: true })
+export type EventListView = z.infer<typeof EventListViewSchema>
+
+export type Event = z.infer<typeof EventSchema>;
