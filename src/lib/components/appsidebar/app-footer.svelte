d5c2e78358c7e8118657a5803ac15b6dad27f919
yihao <yihao039@gmail.com>
2025-12-25 21:31:00 +0800

    update ui

--- src/lib/components/appsidebar/app-footer.svelte
+++ src/lib/components/appsidebar/app-footer.svelte
@@ -8,41 +8,67 @@
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
   import { ChevronUp } from "@lucide/svelte";
   import { session } from "$lib/firebase/session";
+  import { signOut } from "firebase/auth";
+  import Firebase from "$lib/firebase/init";
+  import { goto } from "$app/navigation";
 
-  const user = $derived($session?.user ?? undefined);
-  const username = $derived(user ? user.displayName || user.email : "Login");
+  const username = $derived(
+    $session.loading
+      ? "Loading..."
+      : $session?.user?.displayName || $session?.user?.email || "Login",
+  );
+
+  const handleSignout = async () => {
+    console.log("Signing out...");
+    signOut(Firebase.getAuth());
+    await fetch("/api/auth", {
+      method: "DELETE",
+    });
+    session.set({
+      user: null,
+      loading: false,
+      loggedIn: false,
+    });
+    goto("/login");
+  };
 </script>
 
 <Footer>
   <Menu>
     <MenuItem>
-      <DropdownMenu.Root>
-        <DropdownMenu.Trigger>
-          {#snippet child({ props })}
-            <MenuButton
-              {...props}
-              class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
-            >
-              {username}
-              <ChevronUp class="ms-auto" />
-            </MenuButton>
-          {/snippet}
-        </DropdownMenu.Trigger>
-        <DropdownMenu.Content
-          side="top"
-          class="w-(--bits-dropdown-menu-anchor-width)"
-        >
-          <DropdownMenu.Item>
-            <span>Account</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item>
-            <span>Billing</span>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item>
-            <span>Sign out</span>
-          </DropdownMenu.Item>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
+      {#if $session.loggedIn}
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            {#snippet child({ props })}
+              <MenuButton
+                {...props}
+                class="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+              >
+                {username}
+                <ChevronUp class="ms-auto" />
+              </MenuButton>
+            {/snippet}
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content
+            side="top"
+            class="w-(--bits-dropdown-menu-anchor-width)"
+          >
+            <DropdownMenu.Item>
+              <span>Account</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item>
+              <span>Billing</span>
+            </DropdownMenu.Item>
+            <DropdownMenu.Item onclick={handleSignout}>
+              <span>Sign out</span>
+            </DropdownMenu.Item>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      {:else}
+        <MenuButton>
+          <a href="/login">Log in</a>
+        </MenuButton>
+      {/if}
     </MenuItem>
   </Menu>
 </Footer>
