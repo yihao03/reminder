6445a6c781a8643c625999795d2346eb95299004
yihao <yihao039@gmail.com>
2025-12-25 15:38:31 +0800

    Add default session

--- src/lib/firebase/session.ts
+++ src/lib/firebase/session.ts
@@ -1,4 +1,8 @@
 import type { SessionState } from '$lib/types/auth';
 import { writable, type Writable } from 'svelte/store';
 
-export const session = <Writable<SessionState>>writable();
+export const session = <Writable<SessionState>>writable({
+  user: null,
+  loading: false,
+  loggedIn: false
+});
