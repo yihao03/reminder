86f5ffbb6a557b7c364e4535638fd103ba5ad187
yihao <yihao039@gmail.com>
2025-12-26 22:04:33 +0800

    Move layout to protected route

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,13 @@
+import type { User } from '$lib/types/user';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
   namespace App {
     // interface Error {}
-    // interface Locals {}
+    interface Locals {
+      user: User | null;
+    }
     // interface PageData {}
     // interface PageState {}
     // interface Platform {}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,8 @@
-<script lang="ts">
+<script>
   import "./layout.css";
   import favicon from "$lib/assets/favicon.svg";
-  import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-  import AppSidebar from "$lib/components/appsidebar/app-sidebar.svelte";
-  import type { LayoutProps } from "./$types";
-  import Firebase from "$lib/firebase/init";
-  import { session } from "$lib/firebase/session";
-  import { onAuthStateChanged } from "firebase/auth";
-  import { onMount } from "svelte";
-
-  let { children }: LayoutProps = $props();
-
-  onMount(() => {
-    Firebase.getInstance();
-    const unsubscribe = onAuthStateChanged(Firebase.getAuth(), (user) => {
-      if (user) {
-        session.set({
-          user: {
-            uid: user.uid,
-            displayName: user.displayName,
-            email: user.email,
-            photoURL: user.photoURL,
-          },
-          loading: false,
-          loggedIn: !!user,
-        });
-      } else {
-        session.set({
-          user: null,
-          loading: false,
-          loggedIn: false,
-        });
-      }
-    });
-
-    return unsubscribe; // Cleanup when component unmounts
-  });
+  const { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
-<Sidebar.Provider>
-  <AppSidebar />
-  <main class="flex w-full flex-col">
-    <Sidebar.Trigger />
-    <div class="flex-1 w-full overflow-auto">
-      {@render children?.()}
-    </div>
-  </main>
-</Sidebar.Provider>
+{@render children?.()}
