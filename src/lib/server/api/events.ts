c1ef9ab4449a6d2aad3971bf93caffb9217a5395
yihao <yihao039@gmail.com>
2025-12-26 22:02:49 +0800

    Add event routes

--- src/lib/server/api/events.ts
+++ src/lib/server/api/events.ts
@@ -1,7 +1,12 @@
-import type { EventListView } from "$lib/types/event"
+import type { EventListView, RemindEvent } from "$lib/types/event"
 import { get } from "."
 
 export const getAllEvents = async (customFetch: typeof fetch): Promise<EventListView[]> => {
-  const res = await get<EventListView[]>(customFetch, "/events")
+  const res = await get<EventListView[]>(customFetch, "/events/list")
+  return res.data;
+}
+
+export const getEventById = async (customFetch: typeof fetch, eventId: string) => {
+  const res = await get<RemindEvent>(customFetch, `/admin/events/${eventId}`)
   return res.data;
 }
