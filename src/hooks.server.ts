71044acba6477fd539d44b535b5d09449a9fde53
yihao <yihao039@gmail.com>
2025-12-26 22:04:04 +0800

    Add firebase validation

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,46 @@
 import { BACKEND_URL } from '$env/static/private';
-import { redirect, type Handle, type HandleFetch } from '@sveltejs/kit';
-import { getAuth } from 'firebase-admin/auth';
+import adminAuth from '$lib/server/firebaseadmin/init';
+import { type Handle, type HandleFetch } from '@sveltejs/kit';
+import 'dotenv/config';
+
+import { redirect } from '@sveltejs/kit';
+
+const PUBLIC_ROUTES = ['/api/auth', '/login', '/register'];
 
 export const handle: Handle = async ({ event, resolve }) => {
+  const sessionCookie = event.cookies.get('session');
+  let isAuthenticated = false;
+
+  if (sessionCookie) {
+    isAuthenticated = await adminAuth
+      .verifyIdToken(sessionCookie)
+      .then(() => true)
+      .catch((err) => {
+        console.log("Invalid or expired session cookie.", err.message || err);
+        console.log(`URL: ${event.url.pathname} Referrer: ${event.request.referrer}`);
+        return false;
+      });
+  }
+
+  const isPublicRoute = PUBLIC_ROUTES.some(route =>
+    event.url.pathname.startsWith(route)
+  );
+
+  if (!isAuthenticated && !isPublicRoute) {
+    console.log('Redirecting to login from', event.url.pathname);
+    redirect(303, '/login');
+  }
+
+  if (isAuthenticated && event.url.pathname === '/login') {
+    redirect(303, '/');
+  }
+
   return resolve(event);
-}
+};
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
+  console.info('Handling fetch for request to:', request.url);
   if (request.url.startsWith(BACKEND_URL)) {
-
     const token = event.cookies.get('session');
 
     if (token) {
@@ -18,5 +50,11 @@ export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
     request.headers.set('X-Client-Side', 'SvelteKit');
   }
 
-  return fetch(request);
+  const res = await fetch(request);
+
+  if (res.status === 401) {
+    event.cookies.delete('session', { path: '/' });
+  }
+
+  return res;
 };
