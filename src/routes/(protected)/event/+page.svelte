e49cb5787dbe1f0a87165951f5feadcf7864f985
yihao <yihao039@gmail.com>
2025-12-30 09:20:18 +0800

    Add event list page

---
+++ src/routes/(protected)/event/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+  import EventListItem from "$lib/components/events/event-list-item.svelte";
+
+  const { data } = $props();
+</script>
+
+<div class="w-full flex flex-col">
+  <h1 class="text-4xl font-bold p-8">Events</h1>
+  <div class="space-y-4 mx-auto">
+    {#each data.events as event}
+      <EventListItem {event} />
+    {/each}
+  </div>
+</div>
