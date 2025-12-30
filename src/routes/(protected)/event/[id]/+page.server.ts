1e2c08ddcb4399eb43f672e0d8dcf9db64020aa9
yihao <yihao039@gmail.com>
2025-12-30 09:19:28 +0800

    Redirect if event not found

--- src/routes/(protected)/event/[id]/+page.server.ts
+++ src/routes/(protected)/event/[id]/+page.server.ts
@@ -1,8 +1,15 @@
 import { getEventById } from "$lib/server/api/events";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import type { RemindEvent } from "$lib/types/event";
 
 export const load: PageServerLoad = async ({ fetch, params }) => {
-  const event = await getEventById(fetch, params.id);
+  let event: RemindEvent;
+  try {
+    event = await getEventById(fetch, params.id);
+  } catch (error) {
+    redirect(307, '/event');
+  }
   return {
     event
   }
